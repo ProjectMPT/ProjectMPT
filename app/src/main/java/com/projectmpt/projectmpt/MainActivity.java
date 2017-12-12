c6fe54dce0ad795030cea7280a4ea1cbb8e63477
MadSwede <urbsemail@gmail.com>
2017-12-12 09:51:29 -0800

    New main screenz

--- app/src/main/java/com/projectmpt/projectmpt/MainActivity.java
+++ app/src/main/java/com/projectmpt/projectmpt/MainActivity.java
@@ -41,7 +41,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
-        //Note
 
     }
 
