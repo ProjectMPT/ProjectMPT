a25826d10f342a606c9e291b63718b37414f595a
Urban <urbsemail@gmail.com>
2018-01-17 11:41:40 -0800

    Play style tabs in Meet activity

--- app/src/main/java/com/projectmpt/projectmpt/FirstFragment.java
+++ app/src/main/java/com/projectmpt/projectmpt/FirstFragment.java
@@ -8,35 +8,30 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class FirstFragment extends Fragment {
-    // Store instance variables
-    private String title;
-    private int page;
+    public static final String ARG_PAGE = "ARG_PAGE";
 
-    // newInstance constructor for creating fragment with arguments
-    public static FirstFragment newInstance(int page, String title) {
-        FirstFragment fragmentFirst = new FirstFragment();
+    private int mPage;
+
+    public static FirstFragment newInstance(int page) {
         Bundle args = new Bundle();
-        args.putInt("someInt", page);
-        args.putString("someTitle", title);
-        fragmentFirst.setArguments(args);
-        return fragmentFirst;
+        args.putInt(ARG_PAGE, page);
+        FirstFragment fragment = new FirstFragment();
+        fragment.setArguments(args);
+        return fragment;
     }
 
-    // Store instance variables based on arguments passed
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        page = getArguments().getInt("someInt", 0);
-        title = getArguments().getString("someTitle");
+        mPage = getArguments().getInt(ARG_PAGE);
     }
 
-    // Inflate the view for the fragment based on layout XML
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_first, container, false);
-        TextView tvLabel = (TextView) view.findViewById(R.id.tvLabel);
-        tvLabel.setText(page + " -- " + title);
+        TextView textView = (TextView) view;
+        textView.setText("Fragment #" + mPage);
         return view;
     }
 }
